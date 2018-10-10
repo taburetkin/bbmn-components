c42a1a4805fd26c46e2cdee984cc398113406cc6
dimaver <dima.taburetkin@gmail.com>
2018-10-10 09:43:47 +0300

    app render layout

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import _ from 'underscore';
-import { BaseClass, Events } from 'bbmn-core';
+import { BaseClass, Events, Region, isClass } from 'bbmn-core';
 import { StartableMixin } from '../process/index.js';
-import { mix, getOption, triggerMethod } from 'bbmn-utils';
+import { mix, getOption, triggerMethod, buildViewByKey } from 'bbmn-utils';
 
 const BaseApp = mix(BaseClass).with(Events, StartableMixin);
 
@@ -35,4 +35,53 @@ export default BaseApp.extend({
 		this._startPromises.push(...args);
 	},
 	initialize:() => {},
+	doOnResolve(promise, callback, ...args){
+		return promise.then(() => {
+			return callback.apply(this, args);
+		});
+	},
+	renderLayout(options){
+		if (!this.layoutView) {
+			this.layoutView = this.buildLayout(options);
+			if(!this.layoutView) { return; }
+		}
+		let region = this.getRegion();
+		region.show(this.layoutView);
+	},
+	buildLayout(options){
+		return buildViewByKey(this, 'layout', { options });
+	},
+	getRegion(){
+		if (this._region) return this._region;
+		this._region = this.buildRegion();
+		return this._region;
+	},
+	buildRegion(){
+		let region = this.getOption('region', { args: [this] }) || {};
+		let el = this.getOption('appEl') || 'body';
+		let replaceElement = true;
+		let opts = { el, replaceElement };
+		let RegionClass = Region;
+
+		if (isClass(region, Region)) {
+			RegionClass = region;
+			region = {};
+		} else if (_.isFunction(region)) {
+			let runtime = region.call(this, this);
+			if(isClass(runtime, Region)){
+				RegionClass = runtime;
+				region = {};
+			} else {
+				region = runtime;
+			}
+		}
+		let options = _.pick(_.extend(opts, region), 'regionClass', 'el', 'replaceElement');
+
+		if(isClass(options.regionClass, Region)){
+			RegionClass = options.regionClass;
+			delete options.regionClass;
+		}
+
+		return new RegionClass(options);
+	},
 });
