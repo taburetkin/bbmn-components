990354e7b9b9eb8b9744839b5a13395b5055a6d9
dimaver <dima.taburetkin@gmail.com>
2018-10-18 18:28:01 +0300

    CollectionView now mixed with cssClassModifiers

--- src/views/index.js
+++ src/views/index.js
@@ -3,7 +3,7 @@ import { mix } from 'bbmn-utils';
 import { cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin } from 'bbmn-mixins';
 
 const ExtView = mix(View).with(cssClassModifiersMixin);
-const ExtCollectionVIew = mix(CollectionView).with(customsMixin, emptyFetchMixin, improvedIndexesMixin);
+const ExtCollectionVIew = mix(CollectionView).with(cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin);
 export {
 	ExtView as View,
 	ExtCollectionVIew as CollectionView
