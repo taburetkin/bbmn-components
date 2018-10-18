67b30799bc3b72420be09620ad6c61c9373db027
dimaver <dima.taburetkin@gmail.com>
2018-10-18 15:21:52 +0300

    views extended with destroy mixin

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,9 @@
 import { View, CollectionView } from 'backbone.marionette';
 import { mix } from 'bbmn-utils';
-import { cssClassModifiersMixin, customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin } from 'bbmn-mixins';
+import { cssClassModifiersMixin, customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin, destroyViewMixin } from 'bbmn-mixins';
 
-const ExtView = mix(View).with(cssClassModifiersMixin);
-const ExtCollectionVIew = mix(CollectionView).with(customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin);
+const ExtView = mix(View).with(cssClassModifiersMixin, destroyViewMixin);
+const ExtCollectionVIew = mix(CollectionView).with(customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin, destroyViewMixin);
 export {
 	ExtView as View,
 	ExtCollectionVIew as CollectionView
