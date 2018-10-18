93a87a3f3cb484412cd01d8e4320a5341c1a7842
dimaver <dima.taburetkin@gmail.com>
2018-10-18 15:31:00 +0300

    removed patch mixins from views. there should be only extending mixins.

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,9 @@
 import { View, CollectionView } from 'backbone.marionette';
 import { mix } from 'bbmn-utils';
-import { cssClassModifiersMixin, customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin, destroyViewMixin } from 'bbmn-mixins';
+import { cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin } from 'bbmn-mixins';
 
-const ExtView = mix(View).with(cssClassModifiersMixin, destroyViewMixin);
-const ExtCollectionVIew = mix(CollectionView).with(customsMixin, nextCollectionViewMixin, emptyFetchMixin, improvedIndexesMixin, destroyViewMixin);
+const ExtView = mix(View).with(cssClassModifiersMixin);
+const ExtCollectionVIew = mix(CollectionView).with(customsMixin, emptyFetchMixin, improvedIndexesMixin);
 export {
 	ExtView as View,
 	ExtCollectionVIew as CollectionView
