6ab62207b8fe3ccefaa95327e58bbe7792aba6c3
dimaver <dima.taburetkin@gmail.com>
2018-10-21 16:54:42 +0300

    action now do not expose actionMethod

--- src/actions/action.js
+++ src/actions/action.js
@@ -3,12 +3,29 @@ import { BaseClass } from 'bbmn-core';
 
 import { getByPath } from 'bbmn-utils';
 
+function createExec(actionInstance, actionMethod){
+	return function exec(instance, ...args){
+		let decline = actionInstance.isNotAllowed(instance, args);
+		if (decline) {
+			return actionInstance.onExecuteNotAllowed(instance, decline, args);
+		}
+		
+		if (_.isFunction(actionMethod)) {
+			return actionMethod.apply(instance, args);
+		}
+		else {
+			return actionInstance.onActionMissing(instance);
+		}
+	}
+}
+
+
 const Action = BaseClass.extend({
 	constructor(options = {}){
 		let { name, action } = options;
 		this.options = _.omit(options, 'name', 'callback');
 		this.name = name;
-		this._action = action;
+		this.exec = createExec(this, action);
 	},
 	getOption(key){
 		return getByPath(this.options, key);
@@ -17,20 +34,7 @@ const Action = BaseClass.extend({
 	getAction() {
 		return this._action;
 	},
-	exec(instance, ...args){
-		let decline = this.isNotAllowed(instance, args);
-		if (decline) {
-			return this.onExecuteNotAllowed(instance, decline, args);
-		}
-		let action = this.getAction();
 
-		if (_.isFunction(action)) {
-			return action.apply(instance, args);
-		}
-		else {
-			return this.onActionMissing(instance);
-		}
-	},
 	is (arg) { return this == arg || this.name == arg; },
 	isVisible () { return this.hidden !== true; },
 	isNotAllowed () { },
