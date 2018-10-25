ba07f4817986de4c9b869ed9537856482d01da52
dimaver <dima.taburetkin@gmail.com>
2018-10-25 15:28:48 +0300

    views now has extended getOption and mergeOptions

--- src/views/views.js
+++ src/views/views.js
@@ -1,11 +1,17 @@
 import { View, CollectionView } from 'backbone.marionette';
-import { mix } from 'bbmn-utils';
+import { mix, getOption, mergeOptions } from 'bbmn-utils';
 import { cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin } from 'bbmn-mixins';
 
+const common = {
+	getOption(){
+		return getOption(this, ...arguments);
+	},
+	mergeOptions,
+};
 
-const ExtView = mix(View).with(cssClassModifiersMixin);
+const ExtView = mix(View).with(cssClassModifiersMixin, common);
 
-const ExtCollectionVIew = mix(CollectionView).with(cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin);
+const ExtCollectionVIew = mix(CollectionView).with(cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin, common);
 
 export {
 	ExtView as View,
