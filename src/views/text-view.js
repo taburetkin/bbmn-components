e1a9bb14497735f4b8c412530010f09dcf1ff663
dimaver <dima.taburetkin@gmail.com>
2018-10-28 11:12:03 +0300

    TextView refactoring

--- src/views/text-view.js
+++ src/views/text-view.js
@@ -1,26 +1,26 @@
 import _ from 'underscore';
 import { BackboneView } from 'bbmn-core';
-import { triggerMethod } from 'bbmn-utils';
-import { PropertySchema } from '../model-schemas';
+import { triggerMethod, getOption } from 'bbmn-utils';
+
 
 export const TextView = BackboneView.extend({
+	displayInsteadGet: true,
 	constructor(options = {}){
 		this.options = options;
-		let { text, isHtml = true, property } = options;
-		this.setValue({ text, isHtml, preventRender: true });		
+		let { text, isHtml = true, property, schema, customValue } = options;
+		this.setValue({ text, isHtml, preventRender: true });
+
 		BackboneView.apply(this, arguments);
 		if (this.model && property) {
-			let name;
-			if(_.isString(property)) {
-				name = property;
-			} else if (property instanceof PropertySchema) {
-				this.schema = property;
-				name = property.name;
-			}
-			this.property = name;
+			this.customValue = customValue;
+			this.schema = schema;
+			this.property = property;
 			this.applyPropertyValue();
 			name && this.listenTo(this.model, 'change:' + name, this.applyPropertyValue);
 		}
+	},
+	getOption(){
+		return getOption(this, ...arguments);
 	},
 	render(){
 		this.setNodeValue();
@@ -57,8 +57,14 @@ export const TextView = BackboneView.extend({
 		this.setValue(value);
 	},
 	getPropertyValue(){
+		
+		if (_.isFunction(this.customValue)) {
+			return this.customValue.call(this, this.property, this.model, this);
+		}
+
 		let val = this.model.get(this.property);
-		if (this.schema) {
+		let useDisplay = this.getOption('displayInsteadGet');
+		if (useDisplay && this.schema) {
 			return this.schema.getDisplayValue(val, this.model);
 		} else {
 			return val;
