e3e0c5230428401c75effbca2492113a5038402f
dimaver <dima.taburetkin@gmail.com>
2018-11-18 16:09:54 +0300

    views receives renderOnModelChange

--- src/views/views.js
+++ src/views/views.js
@@ -2,20 +2,51 @@ import { View, CollectionView } from 'backbone.marionette';
 import { mix } from 'bbmn-utils';
 import { cssClassModifiersMixin, customsMixin, emptyFetchMixin, improvedIndexesMixin, optionsMixin } from 'bbmn-mixins';
 
-const ExtView = mix(View)
+
+function _initializeRenderOnModelChange(){
+	let romc = this.getOption('renderOnModelChange', { args: [this.model, this]});
+	if (romc && this.model) {
+		this.listenTo(this.model, 'change', () => this.render());
+	}
+}
+
+
+const BaseView = mix(View)
 	.with(
 		optionsMixin, 
-		cssClassModifiersMixin
+		cssClassModifiersMixin,
+		{
+			_initializeRenderOnModelChange
+		}		
 	);
 
-const ExtCollectionVIew = mix(CollectionView)
+
+
+const ExtView = BaseView.extend({
+	constructor(){
+		BaseView.apply(this, arguments);
+		this._initializeRenderOnModelChange();
+	},
+});
+
+const BaseCollectionVIew = mix(CollectionView)
 	.with(
 		optionsMixin, 
 		cssClassModifiersMixin, 
 		customsMixin, 
 		emptyFetchMixin, 
-		improvedIndexesMixin
+		improvedIndexesMixin,
+		{
+			_initializeRenderOnModelChange
+		}
 	);
+
+const ExtCollectionVIew = BaseCollectionVIew.extend({
+	constructor(){
+		BaseCollectionVIew.apply(this, arguments);
+		this._initializeRenderOnModelChange();
+	},
+});
 
 export {
 	ExtView as View,
