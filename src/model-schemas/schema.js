6d2018fa8fdb7eb286844d64594155ba124eff7e
dimaver <dima.taburetkin@gmail.com>
2018-11-18 14:17:31 +0300

    Schema now supports Events

--- src/model-schemas/schema.js
+++ src/model-schemas/schema.js
@@ -1,9 +1,11 @@
-import { BaseClass } from 'bbmn-core';
-import { getOption } from 'bbmn-utils';
+import { BaseClass, Events } from 'bbmn-core';
+import { getOption, triggerMethod, mix } from 'bbmn-utils';
 
-const Schema = BaseClass.extend({
+const Schema = mix(BaseClass).with(Events).extend({
 	getOption: function(){
 		return getOption(this, ...arguments);
-	}
+	},
+	triggerMethod
 });
+
 export default Schema;
